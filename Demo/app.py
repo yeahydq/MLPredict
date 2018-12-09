a20d39adee29b48eea85ff73948e5985427c3162
Dick Ye <yeahydq@163.com>
2018-12-09 11:00:22 +0800

    add bokeh sample

--- Demo/app.py
+++ Demo/app.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template
 import pandas as pd
+from flask import Flask, render_template
 
-
-# df = pd.read_csv('https://data.boston.gov/dataset/c8b8ef8c-dd31-4e4e-bf19-af7e4e0d7f36/resource/29e74884-a777-4242-9fcc-c30aaaf3fb10/download/economic-indicators.csv',
 df = pd.read_csv('./data/economic-indicators.csv',parse_dates=[['Year', 'Month']])
 length = len(df)
 app = Flask(__name__)
