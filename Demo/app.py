15be256512f48930f871488137fe9566c72a020a
Dick Ye <yeahydq@163.com>
2018-12-09 10:31:08 +0800

    add demo for the first flask job which read the dataframe

--- Demo/app.py
+++ Demo/app.py
@@ -1,16 +1,21 @@
 from flask import Flask, render_template
 import pandas as pd
 
-df = pd.read_csv('https://data.boston.gov/dataset/c8b8ef8c-dd31-4e4e-bf19-af7e4e0d7f36/resource/29e74884-a777-4242-9fcc-c30aaaf3fb10/download/economic-indicators.csv',
-                 parse_dates=[['Year', 'Month']])
+
+# df = pd.read_csv('https://data.boston.gov/dataset/c8b8ef8c-dd31-4e4e-bf19-af7e4e0d7f36/resource/29e74884-a777-4242-9fcc-c30aaaf3fb10/download/economic-indicators.csv',
+df = pd.read_csv('./data/economic-indicators.csv',parse_dates=[['Year', 'Month']])
 length = len(df)
 app = Flask(__name__)
 
 
 @app.route('/')
-def main():
+def index():
     return render_template('index.html', length=length,
                            dataframe=df.to_html())
+
+@app.route('/data')
+def data():
+    return render_template('data.html')
 
 
 if __name__ == '__main__':
